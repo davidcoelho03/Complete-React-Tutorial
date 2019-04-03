326929f187c1b948c4eddf424fcd64f3db245556
David Coelho <a21250080@alunos.isec.pt>
2019-04-03 15:00:13 +0100

    - lesson number 14: added ninjas list with a map iterating through them and returning a view

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas'
 
 class App extends Component {
+  state = {
+    ninjas: [
+      {name: 'David', age: '30', belt: 'brown', id: 1},
+      {name: 'Diogo', age: '24', belt: 'green', id: 2},
+      {name: 'Bruno', age: '26', belt: 'blue', id: 3}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React App</h1>
         <p>Welcome :)</p>
-        <Ninjas name="David" age="22" belt="Brown" /> 
-        <Ninjas name="Bruno" age="22" belt="Brown" /> 
+        <Ninjas ninjas={this.state.ninjas} /> 
       </div>
     );
   }
