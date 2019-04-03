a2857cd8e227a4df4e8e17a78e1c24305b8f3dbc
David Coelho <a21250080@alunos.isec.pt>
2019-04-03 15:27:49 +0100

    - lesson number 16: Conditional output. Just show some ninjas if they correspond to certain conditions

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,23 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-  render(){
-    const { ninjas } = this.props;
-    const ninjaList = ninjas.map(ninja => {
-      return(
-        <div className="ninja" key={ninja.id}>
-          <div>Name: {ninja.name}</div>
-          <div>Age: {ninja.age}</div>
-          <div>Belt: {ninja.belt}</div>
-        </div>
-      ) 
-    });    
+const Ninjas = ({ ninjas }) => {
+  //const  { ninjas } = props; // if we had props in arguments
+
+/*   const ninjaList = ninjas.map(ninja => {
     return(
-      <div className="ninja-list">
-        {ninjaList}
+      <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>Belt: {ninja.belt}</div>
       </div>
-    )
-  }
+    ) 
+  });     */
+
+  const ninjaList = ninjas.map(ninja => {
+    return ninja.age > 20 ? (      
+      <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>Belt: {ninja.belt}</div>
+      </div>
+    ) : null;
+  }); 
+  
+  return(
+    <div className="ninja-list">
+      { ninjaList }
+    </div>
+  )
 }
 
 export default Ninjas;
