2278cd83d305f46ba0a3b88885c41bf02d5229a9
David Coelho <a21250080@alunos.isec.pt>
2019-04-03 15:56:28 +0100

    Forms Revisited. Reformulated the forms so we can chang…

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -14,7 +14,7 @@ const Ninjas = ({ ninjas }) => {
   });     */
 
   const ninjaList = ninjas.map(ninja => {
-    return ninja.age > 20 ? (      
+    return ninja.age > 24 ? (      
       <div className="ninja" key={ninja.id}>
         <div>Name: {ninja.name}</div>
         <div>Age: {ninja.age}</div>
