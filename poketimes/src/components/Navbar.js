5b0dbdf241c1ab20f966bbbbf5f8e3c6bd58dc1c
David Coelho <a21250080@alunos.isec.pt>
2019-04-04 14:28:30 +0100

    - lesson 27 - Programmatic Redirects Introduced withRouter

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, withRouter } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (props) => {
+/*   // without withRouter prints empty object because NavBar isnt included in ROUTER on app.js
+  console.log(props); */
+  
+  setTimeout(() => {
+    props.history.push('./about');
+  }, 2000);
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
@@ -16,4 +22,5 @@ const Navbar = () => {
   )
 }
 
-export default Navbar;
+// supercharging a component. Highorder component
+export default withRouter(Navbar);
