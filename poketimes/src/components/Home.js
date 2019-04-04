b37df1d8739e41b7c0e35474ff0d573fef0000dc
David Coelho <a21250080@alunos.isec.pt>
2019-04-04 18:02:36 +0100

    - lesson no 32 - Importing Images Added one image for each post shown on Home

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Pokeball from '../pokeball.png'
 
 class Home extends Component {
   state = {
@@ -11,7 +12,7 @@ class Home extends Component {
       .then(res => {
         this.setState({
           posts: res.data.slice(0,10)
-        });        
+        });                
       })
   }
   render() {
@@ -20,9 +21,10 @@ class Home extends Component {
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="A pokeball"/>
             <div className="card-content">
               <Link to={'/' + post.id}>
-                <span className="card-title">post.title</span>
+                <span className="card-title red-text">{post.title}</span>
               </Link>
               <p>{post.body}</p>  
             </div>
@@ -35,7 +37,7 @@ class Home extends Component {
       </div>
     );
     return (
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
         {postList}
     </div>
