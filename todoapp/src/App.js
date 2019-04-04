d9103f08f0f0438e8a7dcd8beb2db1c1f34c7506
David Coelho <a21250080@alunos.isec.pt>
2019-04-04 11:55:03 +0100

    - lesson 24 - TodoApp part2 Implemented AddTodo

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
   state = {
@@ -11,11 +12,19 @@ class App extends Component {
   deleteTodo = (id) => {
     this.setState({todos: this.state.todos.filter(todo => todo.id !== id)});
   }
+  addTodo = (todo) => {
+    todo.id = new Date().getMilliseconds();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  }
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}  />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
