e8bcf4ca6a8264ce23c5055437987cfa2664a275
David Coelho <a21250080@alunos.isec.pt>
2019-04-04 11:07:21 +0100

    - lesson 23 - Todo App Added new todoApp react app

---
+++ todoapp/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Todos from './Todos';
+
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play mario kart'}
+    ]
+  }
+  deleteTodo = (id) => {
+    this.setState({todos: this.state.todos.filter(todo => todo.id !== id)});
+  }
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}  />
+      </div>
+    );
+  }
+}
+
+export default App;
