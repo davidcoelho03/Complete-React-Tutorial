627f8cf4c3aea2a4f2bfbcd6e4cd888d7d24f4b1
David Coelho <a21250080@alunos.isec.pt>
2019-04-07 16:27:43 +0100

    - lesson no 33 and more - redux introduction

---
+++ reduxbasics/reduxbasics.js
@@ -0,0 +1,26 @@
+
+// we must add the redux cdn -> https://cdnjs.cloudflare.com/ajax/libs/redux/4.0.1/redux.js
+
+const { createStore } from Redux;
+
+const initState = {
+  todos: [],
+  posts: []
+}
+
+// state -> state of the store
+//need to create initial state
+function myreducer(state = initState, action) {
+  console.log(action, state);
+}
+
+// this creates the store and uses the myreducer as the Reducer
+const store = createStore(myreducer);
+
+const todoAction = {
+  type: 'ADD_TODO',
+  todo: 'buy milk'
+};
+
+// we need to dispatch the action 
+store.dispatch(todoAction);
