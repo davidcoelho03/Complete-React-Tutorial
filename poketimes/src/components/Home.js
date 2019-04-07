5f6745cf718d062005d05319be5a1e3d68dcdf4d
David Coelho <a21250080@alunos.isec.pt>
2019-04-07 21:09:46 +0100

    - lesson no 40 - Mapping State to Props:     -Fixed bug about path importing HOC Rainbow -> ../hoc/Rainbow     -Removed getting data to the states through axios. Data is now fetched     from the redux store;     -Added some data to the rootReducer's state.

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,48 +1,66 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Pokeball from '../pokeball.png'
+import { connect } from "react-redux";
 
 class Home extends Component {
-  state = {
-    posts: []
-  }
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(res => {
-        this.setState({
-          posts: res.data.slice(0,10)
-        });                
-      })
-  }
-  render() {
-    const { posts } = this.state;
-    const postList = posts.length ? (
-      posts.map(post => {
-        return (
-          <div className="post card" key={post.id}>
-            <img src={Pokeball} alt="A pokeball"/>
-            <div className="card-content">
-              <Link to={'/' + post.id}>
-                <span className="card-title red-text">{post.title}</span>
-              </Link>
-              <p>{post.body}</p>  
+
+    /*
+
+    NOT NEEDED. Main concept of redux is to have a central store of data that each component can reach
+    and grab
+
+    // state = {
+    //   posts: []
+    // }
+    // componentDidMount(){
+    //   axios.get('https://jsonplaceholder.typicode.com/posts')
+    //     .then(res => {
+    //       this.setState({
+    //         posts: res.data.slice(0,10)
+    //       });
+    //     })
+    // }*/
+    render() {
+        console.log(this.props);
+
+        const { posts } = this.props;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="post card" key={post.id}>
+                        <img src={Pokeball} alt="A pokeball"/>
+                        <div className="card-content">
+                            <Link to={'/' + post.id}>
+                                <span className="card-title red-text">{post.title}</span>
+                            </Link>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div>
+                <p className="center">No posts yet.</p>
             </div>
-          </div>
+        );
+        return (
+            <div className="container home">
+                <h4 className="center">Home</h4>
+                {postList}
+            </div>
         )
-      })
-    ) : (
-      <div>
-        <p className="center">No posts yet.</p>
-      </div>
-    );
-    return (
-      <div className="container home">
-        <h4 className="center">Home</h4>
-        {postList}
-    </div>
-    )
-  }
+    }
 }
 
-export default Home;
+// getting state of the redux store
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+
+// connect returns a high order component
+// setting the props of the store state on the high order component
+export default connect(mapStateToProps)(Home);
--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -1,6 +1,10 @@
 
 const initState = {
-    posts: []
+    posts: [
+        {id: '1', title: 'Squirtle Laid an Egg', body: 'Lorizzle ipsum dolizzle sit yippiyo, consectetizzle'},
+        {id: '2', title: 'Break yo neck', body: 'Break yo neck, yall funky fresh. Aliquam da bomb massa'},
+        {id: '3', title: 'Pellentesque', body: 'Pellentesque habitant morbi tristique mah nizzle et fo '}
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
